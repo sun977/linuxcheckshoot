1090b24dd26b42cfb0f90aee217c7466535e0325
孙浩博 <1115476069@qq.com>
2025-06-29 16:19:49 +0800

    add main func

--- linuxGun.sh
+++ linuxGun.sh
@@ -2587,6 +2587,7 @@ checkOutlogPack(){
 main() {
     # 检查是否提供了参数
     if [ $# -eq 0 ]; then
+		echoBanner
         usage
         exit 1
     fi
@@ -2639,7 +2640,7 @@ main() {
 # 显示使用帮助
 usage() {
     echo -e "${GREEN}LinuxGun 安全检查工具 v5.0 使用说明${NC}"
-    echo -e "${GREEN}使用方法: ./\$(basename \$0) [选项]${NC}"
+    echo -e "${GREEN}使用方法: ./$0 [option] ${NC}"
     echo -e "${GREEN}可用选项:${NC}"
     echo -e "${GREEN}  -h, --help         显示帮助信息${NC}"
     echo -e "${GREEN}  --firewall         防火墙策略检查${NC}"
